dd92df04405862a86e4cc5445a8a06147f5d7f09
AndersonTM30 <andersondev30@gmail.com>
2022-10-20 22:03:23 -0300

    Criando a rota de exclusão de curso

--- config/routes.php
+++ config/routes.php
@@ -1,10 +1,11 @@
 <?php
 
-
+use Alura\Cursos\Controller\Exclusao;
 use Alura\Cursos\Controller\FormularioInsercao;
 
 
 //  lista de rotas
 return [
-    '/novo-curso'       =>  FormularioInsercao::class
+    '/novo-curso'       =>  FormularioInsercao::class,
+    '/excluir-curso'    =>  Exclusao::class
 ];
